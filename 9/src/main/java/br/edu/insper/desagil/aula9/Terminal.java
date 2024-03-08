ddf141eb8ebc32948da78a427305529188cdb954
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-03-08 07:50:30 -0300

    Update class 9 for 2024-1

--- 9/src/main/java/br/edu/insper/desagil/aula9/Terminal.java
+++ 9/src/main/java/br/edu/insper/desagil/aula9/Terminal.java
@@ -1,18 +1,18 @@
 package br.edu.insper.desagil.aula9;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Terminal {
-	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(() -> {
-			View view = new View();
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            View view = new View();
 
-			JFrame window = new JFrame("Terminal");
-			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			window.setContentPane(view);
-			window.setResizable(false);
-			window.setVisible(true);
-			window.pack();
-		});
-	}
+            JFrame window = new JFrame("Terminal");
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setContentPane(view);
+            window.setResizable(false);
+            window.setVisible(true);
+            window.pack();
+        });
+    }
 }
