3003210665c5c3c5d0c77945b7156c012032b15c
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-03-13 05:11:01 -0300

    Update class 10 for 2024-1

--- 10/src/test/java/br/edu/insper/desagil/aula10/FileManagerTest.java
+++ 10/src/test/java/br/edu/insper/desagil/aula10/FileManagerTest.java
@@ -3,27 +3,27 @@ package br.edu.insper.desagil.aula10;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class FileManagerTest {
-	private FileManager f;
+public class FileManagerTest {
+    private FileManager f;
 
-	@BeforeEach
-	void setUp() {
-		f = new FileManager();
-	}
+    @BeforeEach
+    void setUp() {
+        f = new FileManager();
+    }
 
-	@Test
-	void load() {
-	}
+    @Test
+    void load() {
+    }
 
-	@Test
-	void loadMissing() {
-	}
+    @Test
+    void loadMissing() {
+    }
 
-	@Test
-	void loadInvalid() {
-	}
+    @Test
+    void loadInvalid() {
+    }
 
-	@Test
-	void save() {
-	}
+    @Test
+    void save() {
+    }
 }
