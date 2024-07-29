8481c7d58cbb3f608e2382ed592ec38fd10dde46
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-07-29 16:16:58 -0300

    Update class 2 for 2024-2

--- 2/src/main/java/br/edu/insper/desagil/aula2/Calculadora.java
+++ 2/src/main/java/br/edu/insper/desagil/aula2/Calculadora.java
@@ -2,22 +2,6 @@ package br.edu.insper.desagil.aula2;
 
 public class Calculadora {
     public double executa(int a, int b, char op) {
-        if (op == '+') {
-            return a + b;
-        }
-        if (op == '-') {
-            return a - b;
-        }
-        if (op == '*') {
-            return a * b;
-        }
-        if (op == '/') {
-            try {
-                return a / b;
-            } catch (ArithmeticException exception) {
-                return 0;
-            }
-        }
-        return 0;
+        return 0.0;
     }
 }
