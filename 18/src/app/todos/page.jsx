2afde20919791b439d071ff05b3f93d9778d3a87
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2025-10-20 13:43:54 -0300

    Update class 18 again

--- 18/src/app/todos/page.jsx
+++ 18/src/app/todos/page.jsx
@@ -1,15 +1,35 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 import { GET } from '../_lib/actions';
 
 import styles from './page.module.css';
 
-export default async function Home() {
-    const gatos = await GET('/gatos');
+export default function Home() {
+    const [gatos, setGatos] = useState([]);
+    const [erro, setErro] = useState('');
+
+    useEffect(() => {
+        async function baixarGatos() {
+            try {
+                const resposta = await GET('/gatos');
+                setGatos(resposta);
+            } catch (error) {
+                setErro(error.message);
+            }
+        }
+        baixarGatos();
+    }, []);
 
     return (
         <div className={styles.container}>
-            {gatos.map((gato) => <p key={gato.id}><Link href={'/detalhes/' + gato.id}>{gato.nome}</Link></p>)}
+            {erro ? (
+                <p>{erro}</p>
+            ) : (
+                gatos.map((gato) => <p key={gato.id}><Link href={'/detalhes/' + gato.id}>{gato.nome}</Link></p>)
+            )}
         </div>
     );
 }
